5c2fe7142005a311e7c2178c2e2b9ec0a15aa1bf
sgirishroy <148775275+sgirishroy@users.noreply.github.com>
2023-10-30 21:30:49 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,7 +51,7 @@ def get_fruityvice_data(this_fruit_choice):
   
   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-return fruityvice_normalized
+  return fruityvice_normalized
 
 
 #New section to display the fruityvice api response
