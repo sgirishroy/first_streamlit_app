2553b013dbece10384b4f7218d973e949bddb4cf
sgirishroy <148775275+sgirishroy@users.noreply.github.com>
2023-10-30 21:23:03 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,21 +29,36 @@ streamlit.dataframe(fruits_to_show)
 # import requests
 #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 # fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
-streamlit.header('Fruityvice Fruit Advice')
-fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
-streamlit.write('The user entered', fruit_choice)
+#streamlit.header('Fruityvice Fruit Advice')
+#fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
+#streamlit.write('The user entered', fruit_choice)
 #streamlit.text(fruityvice_response.json()) # just writes the data to the screen
+  
 
 # New section to display the fruityvice api response
 #import requests
 #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
  # takes the json version of the response and normalize it
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+#fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 
  # output it on the screen as a table
-streamlit.dataframe(fruityvice_normalized)
+#streamlit.dataframe(fruityvice_normalized)
+
+
+#New section to display the fruityvice api response
+streamlit.header('Fruityvice Fruit Advice')
+try:
+ fruit_choice = streamlit.text_input('What fruit would you like information about?')
+ if not fruit_choice:
+  streamlit.error('Please select a fruit to get information')
+ else:
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+ streamlit.error()
 
 #dont run anything past here as we trouble shoot
 streamlit.stop()
